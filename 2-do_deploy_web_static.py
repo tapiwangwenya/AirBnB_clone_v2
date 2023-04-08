77fe102401793bafab55ebfc81615e318121fff8
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2023-04-08 04:09:22 -0700

    Air BnB Clone

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.237.115.33", "54.175.120.166"]
 env.user = "ubuntu"
 
 
